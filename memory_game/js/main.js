1ece6cfaee901430c1c21a6d083a0e0e9d94d05e
Josefina Mancilla <jam2012@Josefinas-MBP.attlocal.net>
2017-10-13 14:33:44 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User Flipped" + " " + cardOne);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2){
+   if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You Found a Match!");
+   }
+   else {
+   	alert("Sorry, try again.");
+   }
+}
