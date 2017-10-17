d04bde937b050e6be80a556c1ad96690fe058c0e
Josefina Mancilla <josefinanoemi4@gmail.com>
2017-10-16 21:48:50 -0500

    changed create board function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,35 +21,34 @@ cardImage:	"images/king-of-diamonds.png"}
 var cardsInPlay = [];
 
 var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-};
+}
 
 var flipCard = function() {
 	var cardId = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     this.setAttribute('src', cards[cardId].cardImage);
-    if (cardsInPlay.length === 2) {
-    	checkForMatch();
-    }
+    checkForMatch();
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
-};
+}
 
 
 var createBoard = function(){
-	for (var i = 0; i < cards.length; i ++){
-	var cardElement = document.createElement('img');
-	cardElement.setAttribute('src', "images/back.png");
+    for (var i = 0; i < cards.length; i ++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
     cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild('cardElement');
-	}
-};
+    getElementById('cardElement').addEventListener('click', flipCard);
+    document.getElementbyId('game-board').appendChild('cardElement');
+    }
+}
 
 createBoard();
 
